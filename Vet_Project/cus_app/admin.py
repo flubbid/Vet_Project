d4fb4e0c3f5be9a92c94653457156de297bc3a49
flubbid <brendanwilson1@gmail.com>
2019-06-18 21:44:51 -0500

    registes models to admin.py

--- Vet_Project/cus_app/admin.py
+++ Vet_Project/cus_app/admin.py
@@ -1,3 +1,8 @@
 from django.contrib import admin
-
+from .models import Pet, Customer, Visit, Photo
 # Register your models here.
+
+admin.site.register(Pet)
+admin.site.register(Customer)
+admin.site.register(Visit)
+admin.site.register(Photo)
