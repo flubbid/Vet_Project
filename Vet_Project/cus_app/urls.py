7a6aa9106928418c7745ec1979285ea67d563656
flubbid <brendanwilson1@gmail.com>
2019-06-18 22:11:20 -0500

    adds routs for account setup

--- Vet_Project/cus_app/urls.py
+++ Vet_Project/cus_app/urls.py
@@ -1,6 +1,10 @@
-from django.urls import path
+from django.urls import path, include
 from . import views
 
 urlpatterns= [
     path('', views.home, name='home'),
+    path('about/', views.about, name='about'),
+    path('customers/', views.customers_index, name='cus_index'),
+    path('accounts/', include('django.contrib.auth.urls')),
+    path('accounts/signup', views.signup, name='signup'),
 ]
